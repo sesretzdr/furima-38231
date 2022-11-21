d52487e9f0f338d2b29708bff825756e2f2e4fdb
sesretzdr <3112117229@qq.com>
2022-11-21 23:00:00 +0900

    修正２

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,69 +1,130 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  
-  describe "signup#registration_post" do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
     
-    context "sessionに保存できる" do
+    it '全て正しく入力されれば登録できること' do
+      expect(@user).to be_valid
+    end
 
-      it "全てのカラムが埋まっていれば保存できる" do
-        user = build(:user)
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it 'nicknameがないと登録できない' do
+      @user.nickname = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Nickname can't be blank")
+    end
 
-      it "nicknameが20文字以下であれば保存できる" do
-        user = build(:user, nickname: "1234567890abcdfghijk")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it 'メールアドレスがないと登録できない' do
+      @user.email = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Email can't be blank")
+    end
 
-      it "emailが @ を含む英数字であれば保存できる" do
-        user = build(:user, email: "test@test.com")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it 'メールアドレスが一意性でないと登録できない' do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include('Email has already been taken')
+    end
 
-      it "passwordが7文字以上ならば保存できる" do
-        user = build(:user, password: "tech123")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it 'メールアドレスは@がない登録できない' do
+      @user.email = 'aaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Email is invalid')
+    end
 
-      it "passwordが128文字以下ならば保存できる" do
-        user = build(:user, password: "12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345abcde12345678")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+   
 
-      it "last_nameが35文字以下ならば保存できる" do
-        user = build(:user, last_name: "あかさたなはまやらわあかさたなはまやらわあかさたなはまやらわあかさたな")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it '苗字がない場合は登録できない' do
+      @user.first_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
+    end
 
-      it "first_nameが35文字以下ならば保存できる" do
-        user = build(:user, first_name: "あかさたなはまやらわあかさたなはまやらわあかさたなはまやらわあかさたな")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it '名前がない場合は登録できない' do
+      @user.last_name = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
+    end
 
-      it "kana_last_nameが35文字以下ならば保存できる" do
-        user = build(:user, kana_last_name: "アカサタナハマヤラワアカサタナハマヤラワアカサタナハマヤラワアカサタナ")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    
+    it '氏名カナがない場合は登録できない' do
+      @user.last_name_kana = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
+    end
 
-      it "kana_first_nameが35文字以下ならば保存できる" do
-        user = build(:user, kana_first_name: "アカサタナハマヤラワアカサタナハマヤラワアカサタナハマヤラワアカサタナ")
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it 'last_nameが全角入力でなければ登録できないこと' do
+      @user = build(:user, last_name: "ｱｲｳｴｵ") 
+      @user.valid?
+      expect(user.errors[:last_name]).to include("は不正な値です")
+    end
 
-      it "birthdayがdate型で存在すれば保存できる" do
-        user = build(:user, birthday: Date.new(1990, 04, 02))
-        user.valid?(:registration_post)
-        expect(user).to be_valid
-      end
+    it 'first_nameが全角入力でなければ登録できないこと' do
+      @user = build(:user, first_name: "ｱｲｳｴｵ") 
+      @user.valid?
+      expect(user.errors[:first_name]).to include("は不正な値です")
+    end
 
+   
+
+    it 'last_name_kanaが全角カタカナでなければ登録できないこと' do
+      @user = build(:user, last_name_kana: "あいうえお") 
+      @user.valid?
+      expect(user.errors[:last_name_kana]).to include("は不正な値です")
+    end
+
+    it 'first_name_kanaが全角カタカナでなければ登録できないこと' do
+      @user = build(:user, first_name_kana: "あいうえお")
+      @user.valid?
+      expect(user.errors[:first_name_kana]).to include("は不正な値です")
+    end
+
+   
+  end
+end
+
+describe 'ユーザー新規登録' do
+  context '正常に登録できる時' do
+    it 'パスワードが半角英数混合であれば登録できる' do
+      expect(@user).to be_valid
+    end
+
+    it 'パスワードがないと登録できない' do
+      @user.password = nil
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it 'パスワードが5文字以下では登録できない' do
+      @user.password = 'aaaaa'
+      @user.password_confirmation = 'aaaaa'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+    end
+
+    it 'パスワードは確認用を含めて2回入力しないと登録できない' do
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+    end
+
+
+  end
+
+  context '登録ができない時' do
+    it 'パスワードが英語のみでは登録できない' do
+      @user.password = 'abcdef'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('は半角英数を両方含む必要があります')
+    end
+    it 'パスワードが数字のみでは登録できない' do
+      @user.password = '123456'
+      @user.valid?
+      expect(@user.errors.full_messages).to include('は半角英数を両方含む必要があります')
+    end
+  end
 end
