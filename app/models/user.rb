fafcb5bb234837a5e4bf6ed509756bbf3b69cc1d
sesretzdr <3112117229@qq.com>
2022-11-22 19:23:20 +0900

    正規表現の見直し

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,17 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
    
          validates :nickname, presence: true
-         validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-         validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
-         validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/}
          validates :birth_date, presence: true
+         
+         with_options presence: true, format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+          validates :first_name
+          validates :last_name
+        end
+        with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ } do
+          validates :first_name_kana
+          validates :last_name_kana
+        end
+        
          VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :password, format: { with: VALID_PASSWORD_REGEX }
 end
