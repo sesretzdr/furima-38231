15546c7e5e1b88a886bef3957bad9fbe6ebbbec1
sesretzdr <3112117229@qq.com>
2022-12-02 20:29:23 +0900

    修正１

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -36,7 +36,8 @@ class ProductsController < ApplicationController
     end
   
     def destroy
-      if @product.destroy
+      if current_user.id == @product.user_id
+         @product.destroy
         redirect_to root_path
       else
         redirect :show
